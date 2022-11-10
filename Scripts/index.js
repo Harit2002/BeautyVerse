43d8b5e880f11363f068406220faf37d21fe9984
imvsoni24 <imvsoni24@gmail.com>
2022-11-10 21:58:39 +0530

    dayy3

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,17 +1,10 @@
 
 import { navbar, footer } from "./top_components.js";
 
-// let account = document.getElementById("jsIDnew");
-// account.addEventListener("click", () => {
-//     window.location.href = "./signin.html"
-// })
-
 
 document.getElementById("navbar").innerHTML=navbar();
 
 
 
-document.getElementById("option").addEventListener("click",function(){
-    window.location.href = "/-puny-corn-6420/Products Pages/makeup.html"
-})
+
 
