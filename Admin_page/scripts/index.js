87694b394769189af0e1c071bbb1f398b888c3f2
Harit Khushwas <haritchandel1234@gmail.com>
2022-11-11 12:56:04 +0530

    day-4 admin page

--- Admin_page/scripts/index.js
+++ Admin_page/scripts/index.js
@@ -1,3 +1,8 @@
+import{navbar,leftSideNavbar} from "./nav_components.js"
+
+document.getElementById('LeftSideBar').innerHTML = leftSideNavbar();
+document.getElementById('navbar').innerHTML = navbar();
+
 let name = document.getElementById("name");
 let div = document.getElementById("login");
 let isShown = false;
