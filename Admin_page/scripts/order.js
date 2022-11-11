dc36d3d325270b7247424af80fac07c774813521
Harit Khushwas <112626182+Harit2002@users.noreply.github.com>
2022-11-11 15:10:18 +0530

    Updated Admin page (#31)
    updated admin page

---
+++ Admin_page/scripts/order.js
@@ -0,0 +1,33 @@
+import{navbar,leftSideNavbar} from "./nav_components.js"
+
+document.getElementById('LeftSideBar').innerHTML = leftSideNavbar();
+document.getElementById('navbar').innerHTML = navbar();
+
+let name = document.getElementById("name");
+let div = document.getElementById("login");
+let isShown = false;
+name.addEventListener("click", () => {
+  if (isShown) {
+    div.style.display = "none";
+    isShown = false;
+  } else {
+    div.style.display = "block";
+    isShown = true;
+  }
+
+  console.log(div);
+});
+
+let order_div = document.getElementById('order')
+order_div.style.backgroundColor = "rgb(194, 0, 187)";
+
+let product_div = document.getElementById('product');
+product_div.onclick = () =>{
+    window.location.href = "index.html"
+  }
+
+let home_div = document.getElementById('home');
+home_div.onclick = () =>{
+    window.location.href = "index.html"
+  }
+
